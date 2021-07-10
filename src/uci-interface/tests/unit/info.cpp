bc44a2f998c08f22a84e90c4c18e2ae49f6b4f11
polo123456789 <pabsan02@gmail.com>
2021-07-10 10:08:22 -0600

    (+)<> Added the GSL

--- src/uci-interface/tests/unit/info.cpp
+++ src/uci-interface/tests/unit/info.cpp
@@ -1,5 +1,6 @@
 #include <uci/info.hpp>
 
+#include <gsl/gsl>
 #include <catch2/catch.hpp>
 
 #include <sstream>
@@ -8,7 +9,12 @@ TEST_CASE("uci::info", "[uci] [uci::info]") {
     std::stringstream new_out;
 
     // Redirect std::cout
-    
-    auto* old_cout = std::cout.rdbuf();
+    auto* old_cout_buffer = std::cout.rdbuf();
+    std::cout.rdbuf(new_out.rdbuf());
 
+    auto restore_cout_buffer = gsl::finally([&]() {
+        std::cout.rdbuf(old_cout_buffer);
+    });
+
+    // There are 20 message types, so lets 
 }
