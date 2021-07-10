0c3e91321bd736b357f3950b4e2d9bb76310cb37
polo123456789 <pabsan02@gmail.com>
2021-07-09 19:45:33 -0600

    (+)<uci/info> Werid problem with msvc
    I will look at it another day, now its too late here

---
+++ src/uci-interface/include/uci/info.hpp
@@ -0,0 +1,128 @@
+#ifndef UCI_INTERFACE_INFO_HPP
+#define UCI_INTERFACE_INFO_HPP
+
+#include <uci/globals.hpp>
+
+#include <iostream>
+#include <chrono>
+#include <string>
+#include <type_traits>
+
+//
+// A lot of this is repetetive, so lets use some macros
+//
+
+#define UCI_EXPAND_STRUCT(name, type) \
+    struct name final { \
+        type name; \
+    };
+
+#define UCI_EXPAND_FUNCTION(name, identifyier) \
+    inline void imp_log(const name &message) { \
+        ::std::cout << identifyier " " << message.name << ' '; \
+    }
+
+#define UCI_EXPAND_DEBUG_FUNCTION(name, identifyier) \
+    inline void imp_log(const name &message) { \
+        if (uci::debug) { \
+            ::std::cout << identifyier " " << message.name << ' '; \
+        } \
+    }
+
+#define UCI_EXPAND_NAMESPACED_FUNCTION(nmspace, name, identifyier) \
+    inline void imp_log(const nmspace::name &message) { \
+        ::std::cout << identifyier " " << message.name << ' '; \
+    }
+
+#define UCI_EXPAND_STUCT_AND_FUNCTION(name, type, identifyier) \
+    UCI_EXPAND_STRUCT(name, type) \
+    UCI_EXPAND_FUNCTION(name, identifyier)
+
+
+namespace uci::info {
+
+// ------------------- The direct ones -------------------------------
+
+UCI_EXPAND_STUCT_AND_FUNCTION(depth, size_t, "depth")
+UCI_EXPAND_STUCT_AND_FUNCTION(selective_depth, size_t, "seldepth")
+UCI_EXPAND_STUCT_AND_FUNCTION(nodes, size_t, "nodes")
+UCI_EXPAND_STUCT_AND_FUNCTION(pv, std::string, "pv")
+UCI_EXPAND_STUCT_AND_FUNCTION(multipv, size_t, "multipv")
+UCI_EXPAND_STUCT_AND_FUNCTION(current_move, std::string, "currmove")
+UCI_EXPAND_STUCT_AND_FUNCTION(current_move_number, size_t, "currmovenumber")
+UCI_EXPAND_STUCT_AND_FUNCTION(hashfull, size_t, "hashfull")
+UCI_EXPAND_STUCT_AND_FUNCTION(nodes_per_second, size_t, "nps")
+UCI_EXPAND_STUCT_AND_FUNCTION(table_base_hits, size_t, "tbhits")
+UCI_EXPAND_STUCT_AND_FUNCTION(shredder_base_hits, size_t, "sbhits")
+UCI_EXPAND_STUCT_AND_FUNCTION(cpu_load, size_t, "cpuload")
+UCI_EXPAND_STUCT_AND_FUNCTION(string, std::string, "string")
+UCI_EXPAND_STUCT_AND_FUNCTION(cstring, const char *, "string")
+UCI_EXPAND_STUCT_AND_FUNCTION(refutation, std::string, "refutation")
+UCI_EXPAND_STUCT_AND_FUNCTION(current_line, std::string, "currline")
+
+
+// -------------------     Score     -------------------------------
+
+
+struct score final {
+    UCI_EXPAND_STRUCT(centipawns, size_t)
+    UCI_EXPAND_STRUCT(mate, size_t)
+
+    // This two will need a special function
+    UCI_EXPAND_STRUCT(lowerbound, size_t)
+    UCI_EXPAND_STRUCT(upperbound, size_t)
+};
+
+UCI_EXPAND_NAMESPACED_FUNCTION(score, centipawns, "cp")
+UCI_EXPAND_NAMESPACED_FUNCTION(score, mate, "mate")
+
+inline void imp_log(const score::upperbound &message) {
+    std::cout << "cp " << message.upperbound << " uperbound ";
+}
+
+inline void imp_log(const score::lowerbound &message) {
+    std::cout << "cp " << message.lowerbound << " lowerbound ";
+}
+
+// -------------------      Debug     -------------------------------
+
+
+UCI_EXPAND_STRUCT(debug, std::string)
+UCI_EXPAND_DEBUG_FUNCTION(debug, "string")
+
+UCI_EXPAND_STRUCT(cdebug, const char *)
+UCI_EXPAND_DEBUG_FUNCTION(cdebug, "string")
+
+
+// -------------------       Time      -------------------------------
+
+
+UCI_EXPAND_STRUCT(time, std::chrono::milliseconds)
+
+inline void imp_log(const std::chrono::milliseconds &message) {
+    std::cout << "time " << message.count() << ' ';
+}
+
+// -------------------       Log      -------------------------------
+
+template<class MessageType, class... Arg>
+inline void imp_log(const MessageType& m, Arg... args) {
+    imp_log(m);
+    imp_log(args...);
+}
+
+// NOTE: This one gets called  if the variadic took an odd number of arguments
+inline void imp_log(void) {}
+
+template<class... Messages>
+inline void log(const Messages&... messages) {
+    std::cout << "info ";
+    imp_log(messages...);
+    std::cout << '\n';
+}
+
+} // namespace uci::info
+
+#undef UCI_EXPAND_STUCT_AND_FUNCTION
+
+#endif
---
+++ src/uci-interface/tests/manual/main.cpp
@@ -0,0 +1,13 @@
+#include <uci/info.hpp>
+
+int main(int /*argc*/, char * /*argv*/[]) {
+    uci::info::log(
+        uci::info::depth{10},
+        uci::info::selective_depth{20},
+        uci::info::multipv{1},
+        uci::info::score::centipawns{10},
+        uci::info::nodes{0},
+        uci::info::string{"e2e4 c7c5"}
+    );
+    return 0;
+}
