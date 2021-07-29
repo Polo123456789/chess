d2a0e1cba7912402443a125e2b8013c6e27dc234
polo123456789 <pabsan02@gmail.com>
2021-07-29 10:55:53 -0600

    (+)<uci/option> Public interface

---
+++ src/uci-interface/include/uci/config.hpp
@@ -0,0 +1,6 @@
+#ifndef UCI_INTERFACE_CONFIG_HPP
+#define UCI_INTERFACE_CONFIG_HPP
+
+namespace uci {}
+
+#endif
---
+++ src/uci-interface/include/uci/option.hpp
@@ -0,0 +1,31 @@
+#ifndef UCI_INTERFACE_OPTION_HPP
+#define UCI_INTERFACE_OPTION_HPP
+
+#include <string>
+#include <cstdint>
+
+namespace uci {
+
+class option {
+   public:
+    struct check {
+        using type = bool;
+    };
+    struct spin {
+        using type = int64_t;
+    };
+    struct combo {
+        using type = std::string;
+    };
+    struct string {
+        using type = std::string;
+    };
+
+    template <class Type>
+    typename Type::type as(void);
+};
+
+
+} // namespace uci
+
+#endif
