fca8911ad67cc4e494118f8cd09bc790f508fcaa
kmeeth <87671707+kmeeth@users.noreply.github.com>
2021-08-18 23:00:05 +0200

    A skeleton for the UCI interface class (#4)
    * (~)<.gitignore>Added some Visual Studio stuff to .gitignore
    
    * (~)<uci/info> Fixed some typos and spelling errors
    
    * (+)<uci/engine_interface> Created an .hpp file for the interface.
    
    * (~)<uci/engine_interface> Added header guards.
    
    * (~)<uci/engine_interface>Created the interface with neccessary virtual functions.
    
    * (~)<uci/engine_interface> Added descriptive comments
    
    * (~)<uci/engine_interface.hpp> Included limits.hpp and removed unnecessary namespace prefixes.
    
    * (~)<uci/engine_interface.hpp> Hopefully fixed the inconsistent indentation
    
    * (~)<uci/engine_interface> Fixed the include

---
+++ src/uci-interface/include/uci/engine_interface.hpp
@@ -0,0 +1,58 @@
+#ifndef UCI_ENGINE_INTERFACE_HPP
+#define UCI_ENGINE_INTERFACE_HPP
+
+#include <string>
+
+#include <uci/limits.hpp>
+
+namespace uci {
+	
+    class engine_interface {
+    public:
+        /**
+        * Return whether the automatic register check was passed
+        */
+        virtual bool check_register(void) = 0;
+        /**
+        * Return true if registration is successful
+        */
+	    virtual bool check_register(const std::string &user,
+                                    const std::string &code) = 0;
+        /**
+        * Returns true if copy protection OK 
+        */
+        virtual bool check_copy_protection(void) = 0;
+        /**
+        * Loads default options 
+        */
+        virtual bool load_options(void) = 0;
+        /**
+        * The following are metadata functions
+        */
+        virtual void set_author_name(const char *name) = 0;
+        virtual void set_engine_name(const char *name) = 0;
+        virtual void requires_registration(bool v) = 0;
+        virtual void requires_copy_protection(bool v) = 0;
+        virtual void can_ponder(bool v) = 0;
+        /**
+        * Updates the position the engine holds
+        */
+        virtual void update_position(const std::string &fen,
+                                     const std::string &moves) = 0;
+        /*
+        * Returns the best move in UCI format
+        */
+        virtual std::string get_best_move(const limits &l) = 0;
+        /*
+        * Sets the engine to ponder mode
+        */
+        virtual bool        ponder_mode(void) = 0;
+        /*
+        * Sets the engine to search mode
+        */
+        virtual bool        search_mode(void) = 0;
+	};
+
+}
+
+#endif
