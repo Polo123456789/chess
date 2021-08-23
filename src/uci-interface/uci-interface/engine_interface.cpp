67a2da50abf57eab63850e173cd7f7cf792053e8
kmeeth <87671707+kmeeth@users.noreply.github.com>
2021-08-23 19:39:50 +0200

    (~)<uci/engine_interface>Implemented some simple default functions (#5)

--- src/uci-interface/uci-interface/engine_interface.cpp
+++ src/uci-interface/uci-interface/engine_interface.cpp
@@ -20,3 +20,20 @@ void uci::engine_interface::requires_copy_protection(bool v) {
 void uci::engine_interface::can_ponder(bool v) {
     engine_can_ponder = v;
 }
+
+bool uci::engine_interface::check_register(void) {
+    return true;
+}
+
+bool uci::engine_interface::check_copy_protection(void) {
+    return true;
+}
+
+bool uci::engine_interface::load_options(void) {
+    set_author_name("Unknown Author");
+    set_engine_name("Unknown Engine");
+    requires_registration(false);
+    requires_copy_protection(false);
+    can_ponder(false);
+    return true;
+}
